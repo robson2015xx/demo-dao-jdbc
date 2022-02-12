9c2973adb34eb74968a44f9d05bdc71f75628a67
robson2015xx <robson2015xx@gmail.com>
2022-02-12 12:40:45 -0300

    insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -11,22 +12,27 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Seller seller = sellerDao.findById(3);
-		System.out.println(seller);
+		SellerDao sellerDao = DaoFactory.createSellerDao(); 
+		Seller seller = sellerDao.findById(3); //find by ID
+		System.out.println(seller);	
 		
 		Department dep = new Department(2, null);
-		List<Seller> list = sellerDao.findByDepartment(dep);
+		List<Seller> list = sellerDao.findByDepartment(dep); //find by department
 		
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		list = sellerDao.findAll();
+		list = sellerDao.findAll(); // find all
 		
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id: " + newSeller.getId());
 
 	}
 
